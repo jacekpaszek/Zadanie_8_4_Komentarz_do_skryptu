5f0e55c1be8d321e3dc2c4d395db878b93504113
Jacek <jacek.paszek@onet.pl>
2017-06-03 21:05:37 +0200

    New project

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+/*The script is for learning JavaScript*/
+var name = prompt('Enter your name'); //This is request for name
+// alert('Hello, ' + name);
+console.log('Hello, ' + name); //This is answer from the script in console
