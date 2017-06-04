6bed6b7dd5abb37d244b63b644aee3989d55cbc3
Jacek <jacek.paszek@onet.pl>
2017-06-04 15:49:00 +0200

    JS file modification

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-/*The script is for learning JavaScript*/
+/*
+The script is for learning JavaScript
+Now I'm learning adding comments to the code
+*/
 var name = prompt('Enter your name'); //This is request for name
 // alert('Hello, ' + name);
 console.log('Hello, ' + name); //This is answer from the script in console
